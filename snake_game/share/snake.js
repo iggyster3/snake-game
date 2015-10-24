f504910ead5c09eb32648f4e4097ae67ce5baffc
Richard Ighodaro <richard.ighodaro@hotmail.co.uk>
2015-10-24 10:32:40 +0100

    Adding Snake Class

---
+++ snake_game/share/snake.js
@@ -0,0 +1,3 @@
+var keys = require('./keyboard.js'); // Access Keyboard Class
+var EventEmitter = require('events').EventEmitter; // Access the Event Emitter Class
+var util = require('util'); // Access Node's API
